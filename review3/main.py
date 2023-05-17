784e15872f2e7ac963dd79228a2882815b7e61ca
onionmccabbage <onionmccabbage@live.com>
2023-05-17 14:14:00 +0100

    Sample solution review 3

---
+++ review3/main.py
@@ -0,0 +1,37 @@
+from weather import Weather
+import requests
+import sys
+import datetime
+
+def getWeather(city='Athlone'):
+    key='48f2d5e18b0d2bc50519b58cce6409f1'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={key}'
+    resp = requests.get(url)
+    resp_dict = resp.json()
+    return resp_dict
+
+def buildWeatherInstance(city='Athlone'):
+    weather_dict = getWeather(city)
+    desc = weather_dict['weather'][0]['description']
+    temp = weather_dict['main']['temp']
+    weather_report = Weather(city, desc, temp)
+    print(weather_report)
+    # also write the instances to a text file
+    fout = open('weather.txt', 'at') # append text
+    print(weather_report, file=fout)
+    fout.close()
+
+def main():
+    '''
+    use the Weather class to create instances of weather reports
+    '''
+    # get the default weather (for Athlone)
+    buildWeatherInstance()
+    # if system argument was provided, use it for the city
+    if len(sys.argv)>1:
+        whichCity = sys.argv[1]
+        buildWeatherInstance(whichCity)
+
+if __name__ == '__main__':
+    main()
+
--- review3/weather.py
+++ review3/weather.py
@@ -1,2 +1,54 @@
+from random import randint
+from datetime import datetime # redundancy
+
 class Weather():
-    ''''''
+    '''
+    The Weather class takes a non-empty string for the description
+    and a floating point number for the temperature
+    '''
+    def __init__(self, city, desc, temp):
+        self.city = city
+        self.desc = desc # make use of the setter methods
+        self.temp = temp
+    def getCity(self):
+        return self.__city
+    def setCity(self, new_city):
+        if type(new_city) == str and len(new_city)>2:
+            self.__city = new_city
+        else:
+            self.__city = 'Athlone'
+    def getDesc(self):
+        return self.__desc
+    def setDesc(self, new_desc):
+        if type(new_desc) == str and new_desc != '':
+            self.__desc = new_desc
+        else:
+            self.__desc = 'fine'
+    def getTemp(self):
+        return self.__temp
+    def setTemp(self, new_temp):
+        if type(new_temp) == int or type(new_temp) == float:
+            self.__temp = new_temp
+        else:
+            self.__temp = 12 # a reasonable default
+    def __str__(self):
+        # output a nicely formatted weather report
+        report  = f'The weather in {self.city} is {self.desc} at {self.temp}C'
+        return report
+    city = property(getCity, setCity)
+    desc = property(getDesc, setDesc)
+    temp = property(getTemp, setTemp)
+
+
+if __name__ == '__main__':
+    # exercise this module
+    w_gen = Weather('Genoa', 'rainy', 9.04)
+    w_gal = Weather('Galway', 'windy', 6.70)
+    w_kt = Weather('Kingston', 'sunny', 27.98)
+   
+    print(w_gen)
+    print(w_gal)
+    print(w_kt)
+
+    w_default = Weather(False, [], ()) # wrong data types so should fall back to the defaults
+    print(w_default)
